97750f3f0d3c4a4863a45797752ded9511f7bb0a
Craig Sanders <39004103+c-sanders@users.noreply.github.com>
2025-01-25 15:39:45 +1100

    Minor updates.

--- runLatex.bat
+++ runLatex.bat
@@ -1,3 +1,5 @@
+@echo off
+
 rem --------------------------------------------------------------------------------------------------
 rem
 rem The pdflatex command line utility can probably be found in one or both of the following
@@ -7,10 +9,10 @@ rem   C:\Users\Craig\AppData\Local\Programs\MikTeX\miktex\bin\x64\
 rem
 rem   C:\texlive\2024\bin\windows\
 rem
-rem Please note that the C:\texlive version of this utility resides within the most recently installed
-rem version of this package.
+rem Please note that the most recent version of the pdflatex utility can be found in the installation
+rem which has been installed into the C:\texlive directory.
 rem
 rem --------------------------------------------------------------------------------------------------
 
-@echo off
+
 pdflatex ImaginaryNumbers.tex
